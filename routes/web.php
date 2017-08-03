f185717b1c4bfc529b226c8390f8c4e21ea5c16f
lxg <423749419@qq.com>
2017-08-03 21:50:59 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'oauth', 'namespace' => 'Auth'], function (Router $route) {
+Route::group(['prefix' => 'oauth', 'namespace' => 'Auth'], function ($route) {
     $route->get('/{type}/page', 'OAuthController@page');
     $route->get('/{type}/info', 'OAuthController@info');
     $route->get('/{type}/login', 'OAuthController@login');
